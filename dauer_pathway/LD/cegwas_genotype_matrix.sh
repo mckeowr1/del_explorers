4bdb88224ddc9af4e7861864d3fedaeb2c2be27e
mckeowr1 <68450067+mckeowr1@users.noreply.github.com>
2021-07-20 16:40:07 -0500

    LD Loop , Paired Allele Data, Conservation Code
    Found paired alleles in other dauer genes. Added code to look at conservation of AA position. Made the LD analysis a loop to go through each gene pairing.

--- dauer_pathway/LD/cegwas_genotype_matrix.sh
+++ dauer_pathway/LD/cegwas_genotype_matrix.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
-#SBATCH -J subset      ## Name of job
+#SBATCH -J geno_matrix_loop      ## Name of job
 #SBATCH -A b1042               ## Allocation
 #SBATCH -p genomics                ## Queue
-#SBATCH -t 00:10:00             ## Walltime/duration of the job
-#SBATCH --mem-per-cpu=4G               ## Memory per node in GB needed for a job.
+#SBATCH -t 12:00:00             ## Walltime/duration of the job
+#SBATCH --mem-per-cpu=24G               ## Memory per node in GB needed for a job.
 #SBATCH --cpus-per-task=1            ## Number of processors for your task
 
 
 #Script to go from VCF to Genotype Matrix for DAF-2 AKT-2 Paired alleles
 #CHROM POS REF ALT
 
-
 #module load gatk
 module load bcftools
 
+for filename in /projects/b1059/projects/Ryan/dauer/paired_alleles/*_alleles ;
+
+do
+
+
+
 bcftools view \
--R paired_alleles.tsv \
+-R $filename \
 -Ou /projects/b1059/projects/Ryan/WI.20210121.hard-filter.isotype.vcf.gz |
 #> paired_allele.vcf
 
@@ -36,4 +41,7 @@ bcftools query --print-header -f '%CHROM\t%POS\t%REF\t%ALT[\t%GT]\n' | \
   sed 's/1\/1/1/g'  | \
   sed 's/0\/1/NA/g' | \
   sed 's/1\/0/NA/g' | \
-  sed 's/.\/./NA/g' > Genotype_Matrix.tsv
+  sed 's/.\/./NA/g' > $filename.Genotype_Matrix.tsv
+
+
+done
